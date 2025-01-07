93108e9ee59e3dbd14c33f5bf6dd11b1bcfd4eeb
Jason <jayz71189@gmail.com>
2025-01-07 15:22:53 -0500

    phase 5

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+const validateSignup = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({
@@ -31,7 +50,7 @@ router.post("/", async (req, res) => {
   });
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", validateSignup, async (req, res) => {
   // const hashedPassword = bcrypt.hashSync(password);
   const user = await User.findByPk(req.params.id);
 
