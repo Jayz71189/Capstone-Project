4e1df8419b64632062f9c35aeba7deb1adf5a2f9
Jason <jayz71189@gmail.com>
2025-01-13 18:55:08 -0500

    1st push frontend

---
+++ frontend/src/components/GiftTile.jsx
@@ -0,0 +1,53 @@
+// import { useNavigate } from "react-router-dom";
+import "./GiftTile.css"; // Add styling for individual tiles
+import { useNavigate } from "react-router-dom";
+
+const GiftTile = ({ gift }) => {
+  const { id, name, quantity, description, price, GiftImages } = gift;
+
+  const navigate = useNavigate();
+
+  // previewImage =
+  //   spot.SpotImages.find((image) => image.preview)?.url || "default-image.jpg";
+
+  if (!gift) {
+    return <div>Error: Gift data is undefined</div>;
+  }
+
+  const handleTileClick = () => {
+    navigate(`/gifts/${id}`); // Navigate to the spot's detail page
+  };
+
+  const thumbnailUrl = GiftImages?.[0]?.url || "placeholder-image-url.jpg";
+
+  console.log(GiftImages);
+
+  return (
+    <div
+      className="gift-tile"
+      title={gift.name}
+      onClick={handleTileClick}
+      style={{ cursor: "pointer" }}
+    >
+      <img src={thumbnailUrl} alt={name} />
+      <div className="gift-info">
+        <h3>{name}</h3>
+        <p>{`${description}`}</p>
+        <p>{`${quantity}`}</p>
+        <p>{`$${price} / item`}</p>
+
+        <p>
+          {/* Star Rating with Icon
+          <div className="button-info">
+            <i className="fa fa-star" aria-hidden="true"></i>{" "}
+            {avgRating === null
+              ? "New"
+              : `${parseFloat(Number(avgRating)).toFixed(1)} stars`}
+          </div> */}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GiftTile;
---
+++ frontend/src/store/gifts.js
@@ -0,0 +1,94 @@
+const CREATE_GIFT = "gifts/CREATE_GIFT";
+const UPDATE_GIFT = "gifts/UPDATE_GIFT";
+const DELETE_GIFT = "gifts/DELETE_GIFT";
+
+const createGift = (gift) => ({
+  type: CREATE_GIFT,
+  payload: gift,
+});
+
+const updateGift = (gift) => ({
+  type: UPDATE_GIFT,
+  payload: gift,
+});
+
+const deleteGift = (giftId) => ({
+  type: DELETE_GIFT,
+  payload: giftId,
+});
+
+export const thunkCreateGift = (formData) => async (dispatch) => {
+  const response = await fetch("/api/gifts/create", {
+    method: "POST",
+    body: formData,
+  });
+
+  if (response.ok) {
+    const newGift = await response.json();
+    dispatch(createGift(newGift));
+    return newGift;
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+};
+
+export const thunkUpdateGift =
+  (postId, title, description) => async (dispatch) => {
+    const response = await fetch(`/api/gifts/${giftId}/update`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, description, price, quantity }),
+    });
+
+    if (response.ok) {
+      const updatedGift = await response.json();
+      dispatch(updateGift(updatedGift));
+      return null;
+    } else {
+      const errors = await response.json();
+      return errors;
+    }
+  };
+
+export const thunkDeleteGift = (giftId) => async (dispatch) => {
+  const response = await fetch(`/api/gifts/${giftId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    dispatch(deleteGift(giftId));
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+};
+
+const initialState = { gifts: [] };
+
+const giftsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_GIFT: {
+      return { ...state, gifts: [action.payload, ...state.gifts] };
+    }
+
+    case UPDATE_GIFT: {
+      const updatedGifts = state.gifts.map((gift) =>
+        gift.id === action.payload.id ? { ...gift, ...action.payload } : post
+      );
+      return { ...state, gifts: updatedGifts };
+    }
+
+    case DELETE_GIFT: {
+      const filteredGifts = state.gifts.filter(
+        (gift) => gift.id !== action.payload
+      );
+      return { ...state, gifts: filteredGifts };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default giftsReducer;
