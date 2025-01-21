2ccd25265b72dee5de9abc73b24140ab1cbdd4f4
Jason <jayz71189@gmail.com>
2025-01-20 21:36:16 -0500

    made changes in main

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,12 +3,18 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import { useNavigate } from "react-router-dom";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignupFormModal/SignupFormModal";
+import { useModal } from "../../context/Modal";
 // import CreateGiftModal from "../CreateGiftModal/CreateGiftModal";
 // import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
+  const navigate = useNavigate();
+  const { setModalContent } = useModal();
 
   return (
     <ul>
@@ -20,28 +26,48 @@ function Navigation({ isLoaded }) {
           />
         )}
       </div> */}
+
       <div className="logo">
         <NavLink to="/">
-          <img src="/png.webp" alt="App Logo" className="logo-img" />
+          <img
+            onClick={() => navigate("/")}
+            src="/png.webp"
+            alt="App Logo"
+            className="logo-img"
+          />
         </NavLink>
       </div>
-      <div className="nav-bar">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/likes">My Likes</NavLink>
-        </li>
-        <li>
-          <NavLink to="/comments">My Comments</NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile">My Profile</NavLink>
-        </li>
-        <li>
-          <NavLink to="/purchases">My Purchases</NavLink>
-        </li>
-      </div>
+      {/* Logged-out View */}
+      {!sessionUser ? (
+        <div className="auth-links">
+          <li onClick={() => setModalContent(<LoginFormModal />)}>
+            {" "}
+            &nbsp; Log In
+          </li>
+          <li onClick={() => setModalContent(<SignupFormModal />)}>
+            {" "}
+            &nbsp; Sign Up
+          </li>
+        </div>
+      ) : (
+        <div className="nav-bar">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/likes">My Likes</NavLink>
+          </li>
+          <li>
+            <NavLink to="/comments">My Comments</NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">My Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/purchases">My Purchases</NavLink>
+          </li>
+        </div>
+      )}
 
       {isLoaded && (
         <li className="profile-list">
